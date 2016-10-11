9f7ec9365077f517cb171d415ccb77a0ebf37d1d
xuwei2144@live.cn <xuwei2144@live.cn>
2016-10-11 19:31:31 +0800

    update

--- Scripts/Commen/CheckUserLogin.js
+++ Scripts/Commen/CheckUserLogin.js
@@ -5,7 +5,7 @@
 //用户票据
 //判断用户是否登录，没登录，跳到登录页面，请只在需要的页面添加引用
 var UserInfo = $.cookie("UserInfo") || "";
-if (UserInfo == "") {
+if (UserInfo == "" || UserInfo == "null") {
     window.location.href = "/Course/Views/Account/Login.html"
 }
 console.log(UserInfo);
