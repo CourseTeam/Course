06f1fe20af402804f6dcfffa8e997bba80b0e45b
fuer4869 <fuer4869@gmail.com>
2016-10-21 17:42:22 +0800

    修改已参加课程不能转期

--- Scripts/book/book.js
+++ Scripts/book/book.js
@@ -299,7 +299,7 @@ function create_bookinglist() {
         var disabled = "";
         var btnColor = get_btncolor(row.PhaseStatus);
         if (isNiujin && row.PhaseStatus == 3) {disabled="disabled",btnColor=get_btncolor(4)}
-        if (type == 4) {disabled = "disabled"};
+        if (row.PhaseStatus == 4) {disabled = "disabled"};
         //转期参数
         var param = {"pid": row.PhaseID, "cpname": row.CoursePhaseName};
         var color = isCost ? "#F24D4D" : "#9B9B9B";
