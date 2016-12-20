e3be5a58e44bbf1a910a5bdf0a51eee79a3ec1e0
fuer4869 <NeighborWangShushu@Gmail.com>
2016-12-20 16:04:46 +0800

    管理员端的提现列表构建完成

--- Scripts/Admin/WithdrawList.js
+++ Scripts/Admin/WithdrawList.js
@@ -7,50 +7,87 @@ $(document).ready(function () {
     $("#header").load("../Commen/header.html");
 
     $("#btnSearch").on("click", function () {
-        Withdraw_List(1, 100);
+        Withdraw_List();
     });
-    Withdraw_List(1, 100);
+    $("#Select").on("change", function () {
+        Withdraw_List();
+    });
+    Withdraw_List();
 });
 
+var PageIndex = 1;
+var PageSize = 10;
+
 //课程列表
-function Withdraw_List(PageIndex, PageSize) {
+function Withdraw_List() {
     var SearchKey = $("#SearchKey").val();
     var Status = $("#Select").val();
     var param = {SearchKey: SearchKey, Status:Status, PageIndex: PageIndex, PageSize: PageSize};
-    console.log(param);
     var result = $Course.GetAjaxJson(param, ApiUrl + "Withdraw/Withdraw_List");
     console.log(result);
-    if (result.Msg == "O") {
+    if (result.Msg == "OK") {
         var strHtml = "";
         strHtml += '<li class="list-group-item header">';
         strHtml += '    <div class="row ">';
-        strHtml += '        <div class="col-xs-3">课程名称</div>';
-        strHtml += '        <div class="col-xs-3">课程类型</div>';
-        // strHtml += '        <div class="col-xs-3">课程简介</div>';
-        strHtml += '        <div class="col-xs-2">价格</div>';
-        strHtml += '        <div class="col-xs-4">操作</div>';
-        strHtml += '    </div>';
+        strHtml += '        <div class="col-lg-2">提现人</div>';
+        strHtml += '        <div class="col-lg-2">支付银行</div>';
+        strHtml += '        <div class="col-lg-2">卡号</div>';
+        strHtml += '        <div class="col-lg-2">提现数额</div>';
+        strHtml += '        <div class="col-lg-2">提现人手机</div>';
+        strHtml += '        <div class="col-lg-2">操作</div>';
+        strHtml += '    </div> ';
         strHtml += '</li>';
         if (result.Data.length > 0) {
             for (var i = 0; i < result.Data.length; i++) {
                 var row = result.Data[i];
                 strHtml += '<li class="list-group-item">';
-                strHtml += '    <div class="row ">';
-                strHtml += '        <div class="col-xs-3">' + row.CourseName + '</div>';
-                strHtml += '        <div class="col-xs-3">' + row.CourseType + '</div>';
-                // strHtml += '        <div class="col-xs-3" style="height: 20px;overflow: hidden;" title="' + $Course.DelHtmlTag(row.Intro) + '">点击编辑查看简介</div>';
-                // strHtml += '        <div class="col-xs-3" style="height: 20px;overflow: hidden;" title="' + $Course.DelHtmlTag(row.Intro) + '">' + row.Intro + '</div>';
-                strHtml += '        <div class="col-xs-2">' + row.Tuition + '</div>';
-                strHtml += '        <div class="col-xs-4">';
-                strHtml += '            <button class="autobutton" onclick="Edit(' + row.CourseID + ')">编 辑</button>';
-                strHtml += '            <button class="autobutton" onclick="CourseInfo_Del(' + row.CourseID + ')">删 除</button>';
-                strHtml += '            <button class="autobutton" onclick="Phase_Edit(' + row.CourseID + ')">阶段管理</button>';
-                strHtml += '            <button class="autobutton" onclick="CourseRegistration_List(' + row.CourseID + ',this)" cname = "' + row.CourseName + '">报名表</button>';
+                strHtml += '    <div class="row">';
+                strHtml += '        <div class="col-lg-2">' + row.PayName + '</div>';
+                strHtml += '        <div class="col-lg-2">' + row.BankType + '</div>';
+                strHtml += '        <div class="col-lg-2">' + row.PayNo + '</div>';
+                strHtml += '        <div class="col-lg-2">' + row.Money + '</div>';
+                strHtml += '        <div class="col-lg-2">' + row.Account + '</div>';
+                strHtml += '        <div class="col-lg-2">';
+                if (row.Status == 1) {
+                    strHtml += '            <button class="autobutton" onclick="Withdraw_Finish(' + row.WithID + ')">完成提现</button>';
+                } else {
+                    strHtml += '            <button class="autobutton" style="background-color: gainsboro">已提现</button>';
+                }
                 strHtml += '        </div>';
                 strHtml += '    </div>';
                 strHtml += '</li>';
             }
+            laypage({
+                cont: $("#Page"), //容器。值支持id名、原生dom对象，jquery对象。【如该容器为】：<div id="page1"></div>
+                pages: Math.ceil(result.Data[0].RowsCount / 10), //通过后台拿到的总页数
+                curr: PageIndex || 1, //当前页,
+                skip: true, //是否开启跳页
+                skin: '#AF0000',
+                groups: 3, //连续显示分页数
+                jump: function (obj, first) { //触发分页后的回调
+                    //alert(obj.curr)
+                    if (!first) { //点击跳页触发函数自身，并传递当前页：obj.curr
+                        PageIndex = obj.curr;
+                        Withdraw_List();
+                    }
+                }
+            });
         }
         $("#course_list").html(strHtml);
     }
+}
+
+function Withdraw_Finish(WithID) {
+    layer.confirm("确认已经完成提现？", function () {
+        var param = {WithID: WithID};
+        console.log(WithID);
+        var result = $Course.PostAjaxJson(param, ApiUrl + "Withdraw/Withdraw_Finish");
+        console.log(result);
+        if (result.Msg == "OK") {
+            layer.msg("修改成功", {icon: 1, time: 2000}, function () {
+                Withdraw_List();
+                layer.closeAll();
+            })
+        }
+    });
 }
