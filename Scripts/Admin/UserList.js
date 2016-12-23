71630f880e8a29d13433bf450564bd3610e34fe1
xuwei <xuwei2144@live.cn>
2016-12-23 18:28:48 +0800

    update

--- Scripts/Admin/UserList.js
+++ Scripts/Admin/UserList.js
@@ -29,14 +29,26 @@ function User_list() {
         strHtml += '        <div class="col-xs-2">学校</div>';
         strHtml += '        <div class="col-xs-1">年级</div>';
         strHtml += '        <div class="col-xs-1">班级</div>';
-        strHtml += '        <div class="col-xs-2">手机号码</div>';
+        strHtml += '        <div class="col-xs-1">手机号码</div>';
         strHtml += '        <div class="col-xs-2">家庭住址</div>';
-        strHtml += '        <div class="col-xs-3">操作</div>';
+        strHtml += '        <div class="col-xs-1">用户权限</div>';
+        strHtml += '        <div class="col-xs-2">操作</div>';
         strHtml += '    </div>';
         strHtml += '</li>';
+
         if (result.Data.length > 0) {
             for (var i = 0; i < result.Data.length; i++) {
                 var row = result.Data[i];
+                var role = "无";
+                if (row.Role == 1) {
+                    role = "管理员";
+                }
+                if (row.Role == 2) {
+                    role = "学员";
+                }
+                if (row.Role == 3) {
+                    role = "提现";
+                }
                 strHtml += '<li class="list-group-item">';
                 strHtml += '    <div class="row ">';
                 strHtml += '        <div class="col-xs-1">' + row.NickName + '</div>';
@@ -44,11 +56,12 @@ function User_list() {
                 strHtml += '        <div class="col-xs-2">' + row.School + '</div>';
                 strHtml += '        <div class="col-xs-1">' + row.Grade + '</div>';
                 strHtml += '        <div class="col-xs-1">' + row.ClassName + '</div>';
-                strHtml += '        <div class="col-xs-2">' + row.Phone + '</div>';
+                strHtml += '        <div class="col-xs-1">' + row.Phone + '</div>';
                 strHtml += '        <div class="col-xs-2">' + row.Address + '</div>';
+                strHtml += '        <div class="col-xs-1">' + role + '</div>';
                 strHtml += '        <div class="col-xs-2">';
                 strHtml += '            <button class="autobutton" onclick="Edit(' + row.UserID + ')" >编 辑</button>';
-                strHtml += '            <button class="autobutton" onclick="UserInfo_Del(' + row.UserID + ')">删 除</button>';
+                strHtml += '            <button class="autobutton" onclick="UserRole_Set(' + row.UserID + ')">权限</button>';
                 strHtml += '            <button class="autobutton" onclick="UserInfo_Del(' + row.UserID + ')">删 除</button>';
                 strHtml += '        </div>';
                 strHtml += '    </div>';
@@ -77,6 +90,28 @@ function User_list() {
 
 function Edit(id) {
     window.location.href = "UserEdit.html?UserID=" + id;
+}
+
+function UserRole_Set(UserID) {
+    layer.open({
+        type: 1,
+        skin: "layui-layer-molv",
+        title: "编辑用户权限",
+        content: '<div style="padding: 10px 30px;"><select id="role"><option value="1">管理员</option><option value="3">提现权限</option></select></div>',
+        btn: ["确定", "取消"],
+        area: ["250px", "160px"],
+        yes: function (index) {
+            var param = {UserID: UserID, Role: $("#role").val()};
+            console.log(param);
+            var result = $Course.PostAjaxJson(param, ApiUrl + "User/UserRole_Set");
+            if (result.Msg == "OK") {
+                layer.msg("修改成功！", {icon: 1, time: 2000}, function () {
+                    layer.closeAll();
+                    User_list();
+                });
+            }
+        }
+    });
 }
 
 function UserInfo_Del(id) {
