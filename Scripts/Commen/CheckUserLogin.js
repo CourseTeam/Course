3e15ab7fd8a4046a3335e16940a056229628fd23
xuwei <xuwei2144@live.cn>
2016-12-26 16:16:27 +0800

    update

--- Scripts/Commen/CheckUserLogin.js
+++ Scripts/Commen/CheckUserLogin.js
@@ -6,5 +6,5 @@
 //判断用户是否登录，没登录，跳到登录页面，请只在需要的页面添加引用
 var UserInfo = $.cookie("UserInfo") || "";
 if (UserInfo == "" || UserInfo == "null" || UserInfo == undefined) {
-    window.location.href = "/Course/Views/Account/Login.html"
+    window.location.href = "/Course/Views/Index.html"
 }
